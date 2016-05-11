929905612c7fd0de2cd084adb412c4036883d8dd
AndresPA <andres_pa@outlook.com>
2016-05-11 16:42:14 +0200

    Clase Farmacia

---
+++ Servidor/src/com/pharma/rest/model/Farmacia.java
@@ -0,0 +1,9 @@
+package com.pharma.rest.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+public class Farmacia {
+	
+}
--- Servidor/src/com/pharma/rest/resource/GestorPeticiones.java
+++ Servidor/src/com/pharma/rest/resource/GestorPeticiones.java
@@ -1,10 +1,24 @@
 package com.pharma.rest.resource;
 
+import java.util.ArrayList;
+
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
 
+import com.pharma.rest.model.Farmacia;
 
+@Path("/peticiones")
 public class GestorPeticiones implements Runnable {
+	
+	@Context
+	UriInfo uriInfo;
+	
+	@Context
+	Request request;
+	
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
@@ -12,6 +26,13 @@ public class GestorPeticiones implements Runnable {
 		return "El servidor esta online.";
 	}
 	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public ArrayList<Farmacia> farmaciasCercanas(float x, float y){
+		ArrayList<Farmacia> result = null;
+		return  result;
+	}
 	
 	
 	
